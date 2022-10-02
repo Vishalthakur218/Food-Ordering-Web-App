29e21a59c5830832a1342bcf8dabe93a79f1a886
ShubhankarIntsof <shubhankar.shubham@intsof.com>
2022-10-02 21:40:27 +0530

    final 2

--- MealMonkey/MealMonkey/Controllers/ProductsController.cs
+++ MealMonkey/MealMonkey/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MealMonkey.Models;
 using MealMonkey.ViewModel;
 using Microsoft.AspNet.Identity;
@@ -22,9 +23,6 @@ namespace MealMonkey.Controllers
         // GET: Products
         public ActionResult Index(int? id)
         {
-            int uid = 34;
-            TempData["UserId"] = uid;
-            Session["UserId"] = uid;
 
             var products = db.MM_Products.ToList();
             var categories = mdb.MM_Categories.ToList();
@@ -50,6 +48,7 @@ namespace MealMonkey.Controllers
         // GET: Products/Details/5
         public ActionResult Details(int? id)
         {
+            IsLoggedIn();
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -63,44 +62,24 @@ namespace MealMonkey.Controllers
 
             return View(mM_Products);
         }
-        [Authorize]
-        // GET: Products/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [Authorize]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProductId,Name,Description,Price,Quantity,ImageUrl,CategoryId,IsActive,CreatedDate")] MM_Products mM_Products)
-        {
-            if (ModelState.IsValid)
-            {
-                db.MM_Products.Add(mM_Products);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(mM_Products);
-        }
         [Authorize]
         public ActionResult CartTable()
         {
+            IsLoggedIn();
+            int k = Convert.ToInt32(Session["UserId"]);
             dynamic dy = new ExpandoObject();
-            dy.Carts = getCarts();
+            dy.Carts = getCarts(k);
             dy.Products = getProducts();
 
             return View(dy); 
         }
         [Authorize]
         //Helping Methods
-        public IEnumerable<MM_Carts> getCarts()
+        public IEnumerable<MM_Carts> getCarts(int? K)
         {
-            int K = Convert.ToInt32(Session["UserId"]);
+            IsLoggedIn();
+            //int K = Convert.ToInt32(Session["UserId"]);
             IEnumerable<MM_Carts> Carts = mdb.MM_Carts.ToList();
             var FilteredResult = from s in Carts where s.UserId == K select s;
 
@@ -122,6 +101,7 @@ namespace MealMonkey.Controllers
         //Create cart
         public ActionResult Cart(int? id)
         {
+            IsLoggedIn();
             return View();
         }
 
@@ -133,13 +113,14 @@ namespace MealMonkey.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Cart([Bind(Include ="Cartid,Productid,Quantity,UserId")] MM_Carts mM_Carts)
         {
+            IsLoggedIn();
             MM_Carts temp = new MM_Carts();
 
 
             if (ModelState.IsValid)
             {
                 temp.ProductId = Convert.ToInt32( TempData["ProductId"]);
-                temp.UserId = Convert.ToInt32(Session["UserId"] );
+                temp.UserId = Convert.ToInt32(Session["UserId"]);
                 temp.CartId = mM_Carts.CartId;
                 temp.Quantity = mM_Carts.Quantity;
                 
@@ -157,6 +138,7 @@ namespace MealMonkey.Controllers
         // GET: Products/CartDelete/5
         public ActionResult CartDelete(int? id)
         {
+            IsLoggedIn();
             MM_Carts mM_Cart = mdb.MM_Carts.Find(id);
             mdb.MM_Carts.Remove(mM_Cart);
             mdb.SaveChanges();
@@ -166,7 +148,7 @@ namespace MealMonkey.Controllers
         [Authorize]
         public ActionResult CartToOrder()
         {
-
+            IsLoggedIn();
             int K = Convert.ToInt32(Session["UserId"]);
             IEnumerable<MM_Carts> Carts = mdb.MM_Carts.ToList();
             var FilteredResult = from s in Carts where s.UserId == K select s;
@@ -193,6 +175,7 @@ namespace MealMonkey.Controllers
 
                     mdb.MM_Orders.Add(temp);
                     mdb.SaveChanges();
+
                     while (mdb.MM_Carts.Where(x => x.UserId== K).Count() != 0)
                     {
                         var t = mdb.MM_Carts.Where(x => x.UserId == K).FirstOrDefault();
@@ -210,7 +193,7 @@ namespace MealMonkey.Controllers
         [Authorize]
         public ActionResult OrderList()
         {
-
+            IsLoggedIn();
             int k = Convert.ToInt32(Session["UserId"]);
             IEnumerable<MM_Orders> Orders = mdb.MM_Orders.ToList();
             IEnumerable<MM_Products> Product = db.MM_Products.ToList();
@@ -233,6 +216,29 @@ namespace MealMonkey.Controllers
             return View(OrderList);
 
         }
+        public ActionResult OrderDetails(string id)
+        {
+            int k = Convert.ToInt32(Session["UserId"]);
+            IEnumerable<MM_Orders> mM_Orders = mdb.MM_Orders.ToList();
+            IEnumerable<MM_Products> mM_Products = db.MM_Products.ToList();
+
+            IEnumerable<OrderDetailsViewModel> od = mM_Orders
+                    .Where(u => u.UserId == k && u.OrderNo == id)
+                    .Select(p => new OrderDetailsViewModel
+                    {
+
+                        ProductName = (string) mM_Products.Where(w => w.ProductId == p.ProductId)
+                                            .Select(l => l.Name).FirstOrDefault(),
+                        Quantity = (int)p.Quantity,
+                        Price = (decimal)mM_Products
+                                            .Where(w => w.ProductId == p.ProductId)
+                                            .Select(l => l.Price).FirstOrDefault()
+
+
+                    }).ToList();
+
+            return View(od);
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -241,6 +247,13 @@ namespace MealMonkey.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+        private void IsLoggedIn()
+        {
+            //if(Session["UserId"] == null)
+            //{
+            //    FormsAuthentication.SignOut();
+            //}
         }
     }
 }
